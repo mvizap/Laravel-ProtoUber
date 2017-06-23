989a0bbae53fc0f05a7f0fd6312dc2e99d81891c
Total <evd.totalblood@gmail.com>
2017-06-23 09:13:22 -0400

    Migraciones Listas

--- database/migrations/2017_06_23_130813_create_viajes_table.php
+++ database/migrations/2017_06_23_130813_create_viajes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateViajesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('viajes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('coord_x');
-            $table->string('coord_y');
-            $table->integer('tipo');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('conductor_id');
+            $table->string('inicio_x');
+            $table->string('inicio_y');
+            $table->string('fin_x');
+            $table->string('fin_y');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('viajes');
     }
 }
